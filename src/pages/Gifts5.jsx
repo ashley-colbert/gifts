09cb90baa81bbcffbc4b808c4b71c166b142eaf9
LIS C <acolbert23@yahoo.ca>
2024-11-06 13:42:40 -0330

    vinyl

--- src/pages/Gifts5.jsx
+++ src/pages/Gifts5.jsx
@@ -5,40 +5,40 @@ const Gifts5 = () => {
   return (
     <>
       <div className="gifts2">
-        <h3>Best Advent Calendars of the Season</h3>
+        <h3>Awesome Vinyl Records</h3>
         <div>
-      <a rel="noopener noreferrer" href="https://amzn.to/4dVoCDI" target="blank">
+      <a rel="noopener noreferrer" href="https://amzn.to/3AwFrXq" target="blank">
         <div className="item">
-        <img src="/images/fallwreath.jpg" alt="Door wreath with fall colours and feathers." width="150px"/>
-        <p>Fall Wreath 24 inch Autumn Door Wreath <br></br> Only $34.99</p>
+        <img src="/images/katebush.jpg" alt="An albun cover wtih a drawing of a woman and many animals." width="150px"/>
+        <p>Never For Ever (2018 Remaster)(LP) Kate Bush</p>
         </div>
         </a>
         
-        <a rel="noopener noreferrer" href="https://amzn.to/4efIHEJ" target="blank">
+        <a rel="noopener noreferrer" href="https://amzn.to/4fg2PY3" target="blank">
         <div className="item">
-        <img src="/images/welcome.jpg" alt="Fall themed hanging welcome sign with pumpkins." width="150px"/>
-        <p>Fall Themed Welcome Sign - variety of colours available<br></br> Starting at only $32.99</p>
+        <img src="/images/nevermind.jpg" alt="A blue album cover with a baby underwater chasing a dollar bill." width="150px"/>
+        <p>Nevermind (Vinyl) Nirvana</p>
         </div>
         </a>
 
         <a rel="noopener noreferrer" href="https://amzn.to/3ZjIlZs" target="blank">
         <div className="item">
-        <img src="/images/fallpillow.jpg" alt="Hello Pumpkin throw pillow with black and orange writing." width="150px"/>
-        <p>"Hello Pumpkin" RABUSOFA Throw Pillow Covers - assortment of sizes available<br></br> Starting at only $11.99</p>
+        <img src="/images/blue.jpg" alt="A blue album cover with the members of Weezer standing in a line." width="150px"/>
+        <p>Weezer (Blue Album) (Vinyl) Weezer</p>
         </div>
         </a>
 
-        <a rel="noopener noreferrer" href="https://amzn.to/3XmWjHt" target="blank">
+        <a rel="noopener noreferrer" href="https://amzn.to/40BFcVx" target="blank">
         <div className="item">
-        <img src="/images/candle.jpg" alt="Pumpkin Chai labled candle in a glass holder." width="150px"/>
-        <p>Chloefu LAN Pumpkin Chai Scented Candle<br></br> Only $25.99</p>
+        <img src="/images/dirtydancing.jpg" alt="Dirty Dancing albun cover with a man and a woman dancing." width="150px"/>
+        <p>Dirty Dancing (Original Motion Picture Soundtrack)</p>
         </div>
         </a>
 
-        <a rel="noopener noreferrer" href="https://amzn.to/3XAZ1ZW" target="blank">
+        <a rel="noopener noreferrer" href="https://amzn.to/40xHIvR" target="blank">
         <div className="item">
-        <img src="/images/blockset.jpg" alt="^ characters created by building blocks." width="150px"/>
-        <p>6-piece mini building block set. Note: Found on a US site.</p>
+        <img src="/images/backinblack.jpg" alt="A black album cover with the title AC/DC written at the top." width="150px"/>
+        <p>Back In Black(180 Gram Vinyl) AC/DC</p>
         </div>
         </a>
         </div>
