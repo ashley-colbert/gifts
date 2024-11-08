d227908b85c515d1aa4ba81bead5a7ac5f23b2b0
LIS C <acolbert23@yahoo.ca>
2024-11-08 10:15:14 -0330

    parents

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Gifts2 from './pages/Gifts2';
 import Gifts3 from './pages/Gifts3';
 import Gifts4 from './pages/Gifts4';
 import Gifts5 from './pages/Gifts5';
+import Gifts6 from './pages/Gifts6';
 import Home from './pages/Home';
 import Layout from './pages/Layout';
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="gifts3" element={<Gifts3 />} />
           <Route path="gifts4" element={<Gifts4 />} />
           <Route path="gifts5" element={<Gifts5 />} />
+          <Route path="gifts6" element={<Gifts6 />} />
           <Route path="*" element={<h1>Not Found</h1>} />
         </Route>
       </Routes>
---
+++ src/pages/Gifts6.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import "./Gifts6.css";
+
+const Gifts6 = () => {
+    return (
+        <>
+            <div className="gifts2">
+                <h3>Gifts for Mom and Dad</h3>
+                <div>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/48JfCQC" target="blank">
+                        <div className="item">
+                            <img src="/images/govee.jpg" alt="A box displaying a house with colourful lights in the eves." width="150px" />
+                            <p>Govee Permanent Outdoor Lights</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/4eoX0X4" target="blank">
+                        <div className="item">
+                            <img src="/images/echo.jpg" alt="An echo show displaying a family on the screen." width="150px" />
+                            <p>Echo Show 8 (2nd Gen, 2021 release)</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/3UFbcUM" target="blank">
+                        <div className="item">
+                            <img src="/images/greywolf.jpg" alt="The book cover for The Grey Wolf with a blue background and 2 white wolves." width="150px" />
+                            <p>The Grey Wolf: A Novel by Louise Penny</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/3YVp3c7" target="blank">
+                        <div className="item">
+                            <img src="/images/housemaid.jpg" alt="A book cover for The Housemaid with a blue background and an eye looking through a keyhole." width="150px" />
+                            <p>The Housemaid Paperback by Freida McFadden </p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/4em98rO" target="blank">
+                        <div className="item">
+                            <img src="/images/intoodeep.jpg" alt="Abook cover for In Too eep with a purple and blue background and fire in the distance." width="150px" />
+                            <p>In Too Deep: A Reacher Novel by Lee Child and Andrew Child</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/3CcWL3U" target="blank">
+                        <div className="item">
+                            <img src="/images/birdfeeder.jpg" alt="a red bird sitting on a bird feeder with camera." width="150px" />
+                            <p>Dzees Smart Bird Feeder with Camera, Bird Feeder Camera Wireless Outdoor</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/3UqJaMR" target="blank">
+                        <div className="item">
+                            <img src="/images/elliptical.jpg" alt="A woman using a silver elliptical." width="150px" />
+                            <p>Sunny Health & Fitness Air Walk Cross Trainer Elliptical Machine Glider</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/3ZChvfa" target="blank">
+                        <div className="item">
+                            <img src="images/kodak.jpg" alt="Black and grey Kodak camera with photo being printed from side." width="150px" />
+                            <p>Kodak Mini Shot 2 Retro | 68-Sheet Bundle <br></br> Many packages and sizes available</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/4egbLwh" target="blank">
+                        <div className="item">
+                            <img src="/images/familypjs.jpg" alt="A falimy wearing matching Christmas themed PJs." width="150px" />
+                            <p>IFFEI Family Matching Christmas Pajamas<br></br> All sizes(including pets) and styles available</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/3AwFrXq" target="blank">
+                        <div className="item">
+                            <img src="/images/katebush.jpg" alt="An albun cover wtih a drawing of a woman and many animals." width="150px" />
+                            <p>Never For Ever (2018 Remaster)(LP) Kate Bush</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/4fg2PY3" target="blank">
+                        <div className="item">
+                            <img src="/images/nevermind.jpg" alt="A blue album cover with a baby underwater chasing a dollar bill." width="150px" />
+                            <p>Nevermind (Vinyl) Nirvana</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/3ZjIlZs" target="blank">
+                        <div className="item">
+                            <img src="/images/blue.jpg" alt="A blue album cover with the members of Weezer standing in a line." width="150px" />
+                            <p>Weezer (Blue Album) (Vinyl) Weezer</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/40BFcVx" target="blank">
+                        <div className="item">
+                            <img src="/images/dirtydancing.jpg" alt="Dirty Dancing albun cover with a man and a woman dancing." width="150px" />
+                            <p>Dirty Dancing (Original Motion Picture Soundtrack)</p>
+                        </div>
+                    </a>
+
+                    <a rel="noopener noreferrer" href="https://amzn.to/40xHIvR" target="blank">
+                        <div className="item">
+                            <img src="/images/backinblack.jpg" alt="A black album cover with the title AC/DC written at the top." width="150px" />
+                            <p>Back In Black(180 Gram Vinyl) AC/DC</p>
+                        </div>
+                    </a>
+
+
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Gifts6;
